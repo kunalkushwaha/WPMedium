658249f0bfc6c7c68fbb8c82489ad2420c1a0bef
Charlie Merland <charlie.merland@gmail.com>
2013-02-12 17:20:56 +0100

    Fix #16

--- archive.php
+++ archive.php
@@ -39,7 +39,7 @@ get_header(); $term = get_queried_object(); ?>
 <?php endif; ?>
               <header class="entry-header">
                 <div class="entry-header-image">
-                  <?php wpmedium_the_post_thumbnail(); ?>
+                  <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php wpmedium_the_post_thumbnail(); ?></a>
                 </div>
                 <h1 class="entry-title">
                   <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'wpmedium' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
