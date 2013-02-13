616c992a90abdb0a9bff3ee790b8d9f0cd792e8c
Charlie Merland <charlie.merland@gmail.com>
2013-02-13 16:06:26 +0100

    Responsive design #23

--- inc/js/jquery.wpmedium.js
+++ inc/js/jquery.wpmedium.js
@@ -1,8 +1,11 @@
-jQuery(window).load(function() {
-	jQuery('input#s').focusin(function() {
-		jQuery(this).animate({width: 250}, 250);
-	});
-	jQuery('input#s').focusout(function() {
-		jQuery(this).css({width: ''});
-	});
-});
+// jQuery(window).load(function() {
+// 	if ( jQuery('input#s').length > 0 ) {
+// 		jQuery('input#s').focusin(function() {
+// 			if ( jQuery(this).outerWidth() < 36 )
+// 				jQuery(this).animate({width: 250}, 250);
+// 		});
+// 		jQuery('input#s').focusout(function() {
+// 			jQuery(this).css({width: ''});
+// 		});
+// 	}
+// });
