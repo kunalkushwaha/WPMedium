a0ec7e2fddb9689ee975e9435df7574562e24423
Charlie Merland <charlie.merland@gmail.com>
2013-02-14 13:49:34 +0100

    Resp

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
   <head>
     
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0">
     
     <title><?php is_home() ? printf( '%s | %s', get_bloginfo( 'name' ), get_bloginfo( 'description' ) ) : printf( '%s | %s', wp_title(''), get_bloginfo( 'name' ) ); ?></title>
     
